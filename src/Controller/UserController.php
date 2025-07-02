1266bc6dadb4a9e020759ec7b57c91b6a510260e
kurillos <bocage.cyril@gmail.com>
2025-07-02 11:55:13 +0200

    récupération fichier suite fausse manip sur git

---
+++ src/Controller/UserController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserProfileFormType;
+use App\Form\UserProfilePictureType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Security\Core\USer\UserInterface;
+use App\Form\UserProfilePreferencesType;
+
+final class UserController extends AbstractController
+{
+    #[Route('/profile', name: 'app_user_profile')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function profile(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();//Récupére l'utilisateur connecté
+
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        // Crée le formulaire pour la photo de profil
+        $profilePictureForm = $this->createForm(UserProfilePictureType::class, $user);
+        //dd($_FILES);
+        $profilePictureForm->handleRequest($request);
+
+        if ($profilePictureForm->isSubmitted() && $profilePictureForm->isValid()) {
+            // VichUpload gère automatiquement l'upload de la photo de profil
+            dd($user->getProfilePictureFile());
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre photo de profil a été mise à jour avec succès.');
+
+            return $this->redirectToRoute('app_user_profile');
+        }
+
+        $preferencesForm = $this->createForm(UserProfilePreferencesType::class, $user);
+        $preferencesForm->handleRequest($request);
+
+        if ($preferencesForm->isSubmitted() && $preferencesForm->isValid()) {
+            $entityManager->flush();
+            $this->addFlash('success', 'Vos préférences ont été mises à jour !');
+            return $this->redirectToRoute('app_user_profile');
+        }
+
+        $profileForm = $this->createForm(UserProfileFormType::class, $user);
+        $profileForm->handleRequest($request);
+
+         if ($profileForm->isSubmitted() && $profileForm->isValid()) {
+            $entityManager->flush();
+            $this->addFlash('success', 'Vos informations personnelles ont été mises à jour !');
+            return $this->redirectToRoute('app_user_profile');
+        }
+
+        return $this->render('user/profile.html.twig', [
+            'user' => $user,
+            'profilePictureForm' => $profilePictureForm->createView(),
+            'profileForm' => $profileForm->createView(),
+            'preferencesForm' => $preferencesForm->createView(),
+        ]);
+    }
+}
---
+++ src/Form/UserProfilePictureType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\File;
+
+
+class UserProfilePictureType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('profilePictureFile', fileType::class, [
+                'label' => 'Photo de profil',
+                'mapped' => false,
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Choisissez une photo de profil'
+                ],
+                'constraints' => [
+                    new image([
+                        'maxSize' => '5M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/gif',
+                        ],
+                        'mimeTypesMessage' => 'Veuillez télécharger une image valide (JPEG, PNG, GIF).',
+                        'allowPortrait' => true,
+                        'allowLandscape' => true,
+                    ]),
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
