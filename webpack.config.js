1a2cd193e31e517f19abd646302acad1d2d711fb
kurillos <bocage.cyril@gmail.com>
2025-09-17 10:35:20 +0200

    fix faute de frappe

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,22 @@
 const Encore = require('@symfony/webpack-encore');
 const path = require('path');
 
-if (!Encore.is
-    Production()) {
+if (!Encore.isProduction()) {
     const dotenv = require('dotenv');
     dotenv.config();
-}
-
-if (!Encore.is ). isDefined()) {
 }
 
 Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
+
     .addEntry('app', './assets/app.js')
     .addStyleEntry('main_styles', './assets/scss/custom_bootstrap.scss')
 
     .splitEntryChunks()
-
+    
     .enableStimulusBridge('./assets/controllers.json')
     .enableSingleRuntimeChunk()
-    // VUE: configure la transpilaton du code JavaScript pour les modules ES6
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
         config.corejs = '3.4';
