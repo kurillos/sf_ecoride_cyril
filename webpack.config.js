11e151fd478d6a76f484728d20a1849035de4684
kurillos <bocage.cyril@gmail.com>
2025-09-17 13:14:09 +0200

    suppression de la duplication

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
+
     .addEntry('app', './assets/app.js')
     .addStyleEntry('main_styles', './assets/scss/custom_bootstrap.scss')
     .splitEntryChunks()
@@ -12,17 +13,11 @@ Encore
     .enableSassLoader()
     .enablePostCssLoader()
     .enableSourceMaps(!Encore.isProduction())
-    .enableVersioning(Encore.isProduction());
+    .enableVersioning(Encore.isProduction())
 
-// Config Babel pour Heroku / ES Modules
-Encore.configureBabel((config) => {
-    config.presets.push([
-        '@babel/preset-env',
-        {
-            useBuiltIns: 'usage',
-            corejs: 3
-        }
-    ]);
-});
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    });
 
 module.exports = Encore.getWebpackConfig();
