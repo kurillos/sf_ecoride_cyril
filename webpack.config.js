4a020e91acde14ab97cbdd1edf7265d74ab4deea
kurillos <bocage.cyril@gmail.com>
2025-09-17 10:40:14 +0200

    fix : correction de webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,23 @@
 const Encore = require('@symfony/webpack-encore');
 const path = require('path');
 
-if (!Encore.isProduction()) {
-    const dotenv = require('dotenv');
-    dotenv.config();
-}
+Encore.configureBabel((config) => {
+    config.presets = [
+        ['@babel/preset-env', {
+            useBuiltIns: 'usage',
+            corejs: '3.25'
+        }]
+    ];
+});
 
 Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
-
     .addEntry('app', './assets/app.js')
     .addStyleEntry('main_styles', './assets/scss/custom_bootstrap.scss')
-
     .splitEntryChunks()
-    
-    .enableStimulusBridge('./assets/controllers.json')
     .enableSingleRuntimeChunk()
-    .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = 'usage';
-        config.corejs = '3.4';
-    })
-    .autoProvidejQuery()
+    .enableStimulusBridge('./assets/controllers.json')
     .enableSassLoader()
     .enablePostCssLoader()
     .enableSourceMaps(!Encore.isProduction())
